82194c93c961da76b8b2bfd6da40c034dd2ea812
Randy Zwitch <rzwitch@gmail.com>
2019-06-24 20:12:29 -0400

    Remove binary/http branching logic

--- src/rcpp_main_code.cpp
+++ src/rcpp_main_code.cpp
@@ -5,26 +5,58 @@
 #include <thrift/transport/TTransportUtils.h>
 #include "MapD.h"
 
+
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
 using std::make_shared;
 using std::string;
 
+namespace Rcpp {
+
+    // Defined wrap case
+    template <> SEXP wrap(const TTableDetails& ttd);
+
+}
+
 
 //forward declarations have to come before '#include <Rcpp.h>'
 #include <Rcpp.h>
 using namespace Rcpp;
 
+//mplementation of forward declarations has to come after '#include <Rcpp.h>'
+namespace Rcpp {
+
+  //i Defined wrap case
+  template <> SEXP wrap(const TTableDetails& ttd){
+    
+    //_["partition_detail"] = table_details.partition_detail
+    //_["row_desc"] = table_details.row_desc
+    return List::create(_["fragment_size"] = ttd.fragment_size,
+                        _["page_size"] = ttd.page_size,
+                        _["max_rows"] = ttd.max_rows,
+                        _["view_sql"] = ttd.view_sql,
+                        _["shard_count"] = ttd.shard_count,
+                        _["key_metainfo"] = ttd.key_metainfo,
+                        _["is_temporary"] = ttd.is_temporary);
+  
+  };
+
+}
+
 
 // [[Rcpp::export]]
-List connect(std::string host, int port, std::string user_name, std::string passwd, std::string db_name){
+List connect(std::string host, 
+             int port, 
+             std::string user_name, 
+             std::string passwd, 
+             std::string db_name){
   
   // These have to be shared_ptr per Thrift interface
   auto socket = make_shared<TSocket>(host, port);
   auto transport = make_shared<TBufferedTransport>(socket);
-  auto protocol = make_shared<TBinaryProtocol>(transport);
-  MapDClient* client = new MapDClient(protocol);  //new required to avoid falling out of scope
+  auto pcol = make_shared<TBinaryProtocol>(transport);
+  MapDClient* client = new MapDClient(pcol);  //new required to avoid falling out of scope
   TSessionId session;
   
   //open the transport, which effectively makes the client active
@@ -34,15 +66,16 @@ List connect(std::string host, int port, std::string user_name, std::string pass
   client->connect(session, user_name, passwd, db_name);
   
   //bundle up info as pointers, return to R in List
+  //Per Matt Pulver, is this scoped right or am I getting lucky?
   TTransport* tt = transport.get();  //needs raw, not shared_ptr
   XPtr<TTransport> ptt(tt, true);
   
   XPtr<MapDClient> pmdc(client, true);
-
+  
   return List::create(_["transport"] = ptt,
                       _["client"]    = pmdc,
                       _["sessionId"] = session
-                      );
+  );
   
 }
 
@@ -69,17 +102,7 @@ List get_table_details(List conn, std::string table_name) {
   client->get_table_details(table_details, sessionId, table_name);
   
   //for debugging
-  std::cout << table_details << std::endl;
+  //std::cout << table_details << std::endl;
   
-  //parse table_details once code works
-  //_["partition_detail"] = table_details.partition_detail
-  //_["row_desc"] = table_details.row_desc
-  return List::create(_["fragment_size"] = table_details.fragment_size,
-                      _["page_size"] = table_details.page_size,
-                      _["max_rows"] = table_details.max_rows,
-                      _["view_sql"] = table_details.view_sql,
-                      _["shard_count"] = table_details.shard_count,
-                      _["key_metainfo"] = table_details.key_metainfo,
-                      _["is_temporary"] = table_details.is_temporary);
-  
+  return(Rcpp::wrap(table_details));
 }
