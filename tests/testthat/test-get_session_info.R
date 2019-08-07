81eb03c683e57a16796d40a1613e41c856d85693
Randy Zwitch <rzwitch@gmail.com>
2019-08-07 10:34:45 -0400

    Clean up tests and docs

--- tests/testthat/test-get_session_info.R
+++ tests/testthat/test-get_session_info.R
@@ -3,6 +3,6 @@ test_that("Connect to OmniSci on localhost and get session info", {
   conn <- connect("localhost", 6274, "admin", "HyperInteractive", "omnisci")
   gsi <- get_session_info(conn)
 
-  expect_true(gsi$user == "mapd")
+  expect_true(gsi$user == "admin")
 
 })
