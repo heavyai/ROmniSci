feb3a051f81b14319ddf11979eb7299c6b0db0e1
Randy Zwitch <rzwitch@gmail.com>
2019-09-10 20:07:30 -0400

    Add test for switch_database

---
+++ tests/testthat/test-switch_database.R
@@ -0,0 +1,11 @@
+test_that("Connect to OmniSci on localhost, create, then switch database", {
+  
+  conn <- connect("0.0.0.0", 6274, "admin", "HyperInteractive", "omnisci")
+  
+  sql_execute(conn, "create database if not exists romnisci (owner='admin')")
+  expect_error(switch_database(conn, "romnisci"), NA)
+  
+  #clean up after 
+  sql_execute(conn, "drop database if exists romnisci")
+
+})
