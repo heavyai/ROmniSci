82bdf98641fec0865d95a6144085ff02eb6ce47e
Randy Zwitch <rzwitch@gmail.com>
2020-01-20 14:59:07 -0500

    Stop for now
    Need to figure out how to squash two arrays into one

--- src/thrift_struct_wrap.cpp
+++ src/thrift_struct_wrap.cpp
@@ -42,7 +42,7 @@ namespace Rcpp {
                           _["key_metainfo"] = x.key_metainfo,
                           _["is_temporary"] = x.is_temporary,
                           _["partition_detail"] = static_cast<int>(x.partition_detail) //enum
-                          
+
       );
 
     };
@@ -56,35 +56,35 @@ namespace Rcpp {
                           _["edition"] = x.edition,
                           _["host_name"] = x.host_name,
                           _["poly_rendering_enabled"] = x.poly_rendering_enabled,
-                          _["role"] = static_cast<int>(x.role) //enum    
+                          _["role"] = static_cast<int>(x.role) //enum
       );
 
     };
-    
+
     template <> SEXP wrap(const TSessionInfo& x){
-      
+
       return List::create(_["user"] = x.user,
                           _["database"] = x.database,
                           _["start_time"] = static_cast<Datetime>(x.start_time)
       );
-      
+
     };
-    
+
     template <> SEXP wrap(const TDBInfo& x){
-      
+
       return List::create(_["db_name"] = x.db_name,
                           _["db_owner"] = x.db_owner);
-      
+
     };
 
     template <> SEXP wrap(const TClusterHardwareInfo& x){
-      
+
       return List::create(_["hardware_info"] = x.hardware_info);
-      
+
     };
-    
+
     template <> SEXP wrap(const THardwareInfo& x){
-      
+
       return List::create(_["num_gpu_hw"] = x.num_gpu_hw,
                           _["num_cpu_hw"] = x.num_cpu_hw,
                           _["num_gpu_allocated"] = x.num_gpu_allocated,
@@ -92,20 +92,20 @@ namespace Rcpp {
                           _["host_name"] = x.host_name,
                           _["gpu_info"] = x.gpu_info
                           );
-      
+
     };
-    
+
     template <> SEXP wrap(const TGpuSpecification& x){
-      
+
       return List::create(_["num_sm"] = x.num_sm,
                           _["clock_frequency_kHz"] = makeInt64(x.clock_frequency_kHz),
                           _["memory"] = makeInt64(x.memory),
                           _["compute_capability_major"] = x.compute_capability_major,
                           _["compute_capability_minor"] = x.compute_capability_minor
                          );
-      
+
     };
-    
+
     template <> SEXP wrap(const TTableMeta& x){
 
       std::vector<TTypeInfo> dtypes;
@@ -125,7 +125,7 @@ namespace Rcpp {
                           _["col_names"] = x.col_names
                          );
     };
-    
+
     template <> SEXP wrap(const TDatumVal& x){
 
       return List::create(_["int_val"] = makeInt64(x.int_val),
@@ -140,15 +140,15 @@ namespace Rcpp {
       return List::create(_["val"] = x.val,
                           _["is_null"] = x.is_null
                           );
-      
+
     };
-    
+
     template <> SEXP wrap(const TRow& x){
-      
+
       return List::create(_["cols"] = x.cols);
-      
+
     };
-    
+
     template <> SEXP wrap(const TColumnData& x){
 
       return List::create(_["int_col"] = makeInt64(x.int_col),
@@ -156,33 +156,67 @@ namespace Rcpp {
                           _["str_col"] = x.str_col,
                           _["arr_col"] = x.arr_col
                           );
-      
+
     };
-    
+
     template <> SEXP wrap(const TColumn& x){
 
       return List::create(_["data"] = x.data,
                           _["nulls"] = x.nulls);
-      
+
     };
-    
+
     template <> SEXP wrap(const TRowSet& x){
-      
+
       return List::create(_["row_desc"] = x.row_desc,
                           _["rows"] = x.rows,
                           _["columns"] = x.columns,
                           _["is_columnar"] = x.is_columnar
                           );
-      
+
     };
-    
+
     template <> SEXP wrap(const TQueryResult& x){
-      
+
       return List::create(_["row_set"] = x.row_set,
                           _["execution_time_ms"] = makeInt64(x.execution_time_ms),
                           _["total_time_ms"] = makeInt64(x.total_time_ms),
                           _["nonce"] = x.nonce
                           );
+
+      //1. get number of columns from result
+      int numcols = x.row_set.columns.size();
+
+      //2. for each column, get column name and (data, nulls)
+      List result;
+      for (int i; i < numcols; i++) {
+
+        //get relevant source data from struct
+        auto col_name = x.row_set.row_desc[i].col_name;
+        TColumnData datacol = x.row_set.columns[i].data;
+
+        //find which field in TColumnData has data in it
+        //this doesn't actually work, data scope lost after loop
+        if(datacol.int_col.size() > 0){
+          auto data = datacol.int_col;
+        } else
+        if (datacol.real_col.size() > 0){
+          auto data = datacol.real_col;
+        } else
+        if (datacol.str_col.size() > 0){
+          auto data = datacol.str_col;
+        } else {
+          auto data = datacol.arr_col;
+        }
+
+        //need a function to collapse 'data' and 'nullcol' into single column
+        auto nullscol = x.row_set.columns[i].nulls;
+
+        result.push_back(datacol, col_name);
+      }
+
+      return result;
+
     };
-    
+
 } //end namespace
